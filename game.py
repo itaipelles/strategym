d6b1c4f12507e68af8172435b04ec8e77d827bee
Itai Pelles <pelles.itai@gmail.com>
2023-04-22 17:02:09 +0300

    small things

--- game.py
+++ game.py
@@ -3,21 +3,23 @@ import numpy as np
 import gymnasium as gym
 from gymnasium import spaces
 
-territories = [1,2,3,4,5,6]
+territories = [0,1,2,3,4,5]
 adjacencies = [
-    [2,3], # 1
-    [1,4], # 2
+    [1,2], # 0
+    [0,3], # 1
+    [0,4], # 2
     [1,5], # 3
-    [2,6], # 4
-    [3,6], # 5
-    [4,5] # 6
+    [2,5], # 4
+    [3,4] # 5
 ]
 total_num_of_adjacencies = np.array(adjacencies).size
 movement_action_space = spaces.Box(low=0.0, high=1.0, shape=(total_num_of_adjacencies,))
 
 MAX_INFANTRY_PER_TERRITORY = 50
-discrete_shape = MAX_INFANTRY_PER_TERRITORY*np.ones((len(territories),2))
+discrete_shape = MAX_INFANTRY_PER_TERRITORY*np.ones((2, len(territories)))
 observation_space = spaces.MultiDiscrete(discrete_shape)
+
+opening_observation = [[10,2,2,0,0,0], [0,0,0,2,2,10]]
 
 class AxisAndAlliesGame(gym.Env):
     def __init__(self):
@@ -26,17 +28,17 @@ class AxisAndAlliesGame(gym.Env):
     def reset(self, seed=None, options=None):
         # We need the following line to seed self.np_random
         super().reset(seed=seed)
-        observation = None
+
+        self.observation = np.copy(opening_observation)
         info = None
-        return observation, info
+        return self.observation, info
     
     def step(self, action):
         terminated = False
         reward = 1
-        observation = None
         info = None
 
-        return observation, reward, terminated, False, info
+        return self.observation, reward, terminated, False, info
 
 game = AxisAndAlliesGame()
 obs, info = game.reset()
@@ -47,5 +49,3 @@ print('random action: ', action)
 
 observation, reward, terminated, truncated, info = game.step(action)
 print('observation after step: ', observation)
-
-print('random observation: ', observation_space.sample())
