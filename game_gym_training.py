c358bd4b21bbd06991eb0e448968c7a3b13a0e1c
yaniv amichy <yanivamichy@gmail.com>
2023-04-29 17:44:09 +0300

    new training script, requires better reward structure for learning

--- game_gym_training.py
+++ game_gym_training.py
@@ -1,21 +1,22 @@
-import gym
-import json
-import datetime as dt
-from stable_baselines3.common.vec_env import DummyVecEnv
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
+from game_selfPlay import AxisAndAlliesEnv_selfPlay
+from game_selfPlay import SelfPlayCallback
 from game import AxisAndAlliesEnv
-import pandas as pd
 import time
+import math
 
 if __name__ == "__main__":
-    # our env obs and actions are not properly compatible yet, run this script to see problems
-    # also, DummyVecEnv vs make_vec_env??
-    env = make_vec_env(AxisAndAlliesEnv, n_envs=4) 
+    env = make_vec_env(AxisAndAlliesEnv_selfPlay, n_envs=4) 
+    model = PPO("MultiInputPolicy", env, verbose=1)
+    update_AI_callback = SelfPlayCallback(env,model, updateRate=math.inf)
 
-    model = PPO("MlpPolicy", env, verbose=1)
-    model.learn(total_timesteps=20000)
+    model.learn(total_timesteps=200000, callback=update_AI_callback)
+    model.save("test_model")
+    del model
 
+    # run test
+    model = PPO.load("test_model", env=env)
     obs = env.reset()
     for i in range(2000):
         action, _states = model.predict(obs)
