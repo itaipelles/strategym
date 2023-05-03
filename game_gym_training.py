b8462edf1709c63d934490ae8ba819dcfdc22a5e
yaniv amichy <yanivamichy@gmail.com>
2023-05-03 15:38:00 +0300

    Added configuration for number of layer/neurons and cpu/gpu usage

--- game_gym_training.py
+++ game_gym_training.py
@@ -7,20 +7,21 @@ import time
 import math
 
 if __name__ == "__main__":
-    env = make_vec_env(AxisAndAlliesEnv_selfPlay, n_envs=4) 
-    model = PPO("MultiInputPolicy", env, verbose=1)
+    env = make_vec_env(AxisAndAlliesEnv_selfPlay, n_envs=4)
+    policy_kwargs = dict(net_arch=dict(pi=[128, 128], vf=[128, 128]))
+    model = PPO("MultiInputPolicy", env, verbose=4, n_steps=512, batch_size=64, policy_kwargs=policy_kwargs, device='auto')
     # model = PPO.load("test_model", env=env)
     update_AI_callback = SelfPlayCallback(env,model, updateRate=math.inf)
 
-    model.learn(total_timesteps=10000000, callback=update_AI_callback)
+    model.learn(total_timesteps=100000, callback=update_AI_callback)
     model.save("test_model")
     del model
 
     # run test
+    env = AxisAndAlliesEnv_selfPlay()
     model = PPO.load("test_model", env=env)
-    obs = env.reset()
+    obs, _ = env.reset()
     for i in range(2000):
         action, _states = model.predict(obs)
-        obs, rewards, done, info = env.step(action)
+        obs, rewards, done, truncated, info = env.step(action, render_mid_round=True)
         env.render()
-        time.sleep(0.5)
