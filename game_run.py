b9c2bcadef4f50c3b610104cc51b8fd0f643ff17
yaniv amichy <yanivamichy@gmail.com>
2023-05-07 19:13:53 +0300

    rename default model file name

--- game_run.py
+++ game_run.py
@@ -2,7 +2,7 @@ from stable_baselines3 import PPO
 from AxisnAlliesEnv_selfPlay import AxisAndAlliesEnv_selfPlay
 
 env = AxisAndAlliesEnv_selfPlay()
-model = PPO.load("test_model2", env=env,device='cpu')
+model = PPO.load("game_model", env=env,device='cpu')
 obs, _ = env.reset()
 for i in range(2000):
     action, _states = model.predict(obs)
