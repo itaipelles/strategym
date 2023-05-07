5bd5b1818f9bc28a4140c10fde33c445ec055335
yaniv amichy <yanivamichy@gmail.com>
2023-05-07 13:32:58 +0300

    added int typinh

--- battle_calculator.py
+++ battle_calculator.py
@@ -5,13 +5,13 @@ INFANTRY_ATTACK_VALUE = 2
 INFANTRY_DEFENCE_VALUE = 2
 
 
-def get_dmg(attack_value):
+def get_dmg(attack_value :int):
     dmg = attack_value // 6
     left_over_attack = attack_value % 6
     dmg += (random.randint(1, 6) <= left_over_attack)
     return dmg
 
-def infantry_battle(attacker_inf, defender_inf):
+def infantry_battle(attacker_inf : int, defender_inf : int):
     while attacker_inf>0 and defender_inf>0:
         attack_dmg = get_dmg(attacker_inf*INFANTRY_ATTACK_VALUE)
         defence_dmg = get_dmg(defender_inf*INFANTRY_DEFENCE_VALUE)
